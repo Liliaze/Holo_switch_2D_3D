ff819d39ef5fc422524841ca9a5c104a96ebb479
Liliaze <diana.boudy@gmail.com>
2017-01-13 16:42:35 +0100

    add Assets and Project Settings

---
+++ Assets/Scripts/GoBackToEarth.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System;
+
+public class GoBackToEarth : MonoBehaviour {
+
+	public static Action CloseThisHolographicView;
+	// Use this for initialization
+
+	void OnSelect () {
+		if (!this.GetComponent<Rigidbody>())
+		{
+			CloseThisHolographicView();
+		}
+	}
+
+}
